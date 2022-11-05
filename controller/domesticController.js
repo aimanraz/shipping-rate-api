b09d41452e7ed7d4bf70c072d93e001631c67c05
aimanraz <muhamad.aimanrzq@gmail.com>
2022-11-05 14:30:50 +0800

    prettify json data

--- controller/domesticController.js
+++ controller/domesticController.js
@@ -43,7 +43,7 @@ exports.createAndSend = (req, res) => {
       fromDB.push(newReq);
       fs.writeFile(
         './dev-data/data/saved-data.json',
-        JSON.stringify(fromDB),
+        JSON.stringify(fromDB, null, 2),
         (err) => {
           res.status(201).json({
             status: 'success',
