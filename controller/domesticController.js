a0f8eee93f955d000b57128ad9ec594372eaf495
aimanraz <muhamad.aimanrzq@gmail.com>
2022-11-05 14:05:32 +0800

    some code refactor and cache file requested

--- controller/domesticController.js
+++ controller/domesticController.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const fromDB = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/saved-data.json`, 'utf-8')
 );
+console.log();
 
 exports.getAllRates = (req, res) => {
   if (!fromDB.length) {
@@ -25,36 +26,17 @@ exports.getAllRates = (req, res) => {
 
 exports.createAndSend = (req, res) => {
   const body = req.body;
-  const options = {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
-  };
-  const getRate = async () => {
-    try {
-      const response = await fetch(
-        'https://www.citylinkexpress.com/wp-json/wp/v2/getShippingRate',
-        options
-      );
-      const data = await response.json();
-      console.log(data);
-      req.requestedData = data;
-      return 'Successfully requested!';
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  };
   (async () => {
     try {
-      const requested = await getRate();
+      const requested = await getRate(req, body);
+      console.log(requested);
       if (!req.requestedData) {
         return res.status(404).json({
           status: 'fail',
           result: 'there is no data in database',
         });
       }
-      console.log(requested);
+      fromDB.push(req.requestedData.req.data);
       res.status(201).json({
         status: 'success',
         request: req.requestedData.req,
@@ -68,8 +50,26 @@ exports.createAndSend = (req, res) => {
 exports.refactorInput = (req, res, next) => {
   const requestRate = req.body;
   const instruc = false;
-  instruc
-    ? console.log('Middleware do nothing')
-    : console.log('Middleware working');
+  console.log('Middleware working');
   next();
+};
+
+const getRate = async (req, body) => {
+  try {
+    const options = {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
+    };
+    const response = await fetch(
+      'https://www.citylinkexpress.com/wp-json/wp/v2/getShippingRate',
+      options
+    );
+    const data = await response.json();
+    req.requestedData = data;
+    return 'Successfully requested!';
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
